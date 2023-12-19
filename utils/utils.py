f1b5b512cf9083dae34f841d02bf75ab9138b24c
TastyBread123 <117034601+TastyBread123@users.noreply.github.com>
2023-12-19 18:58:19 +0300

    v3.0

--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,8 @@
 from configs.lessons_config import *
 
+
 def get_text_list_lessons_with_code():
     ret_text = ''
-
-    for i in lessons:
-        ret_text += f'{i} - {lessons[i]}\n'
+    for i in lessons: ret_text += f'{i} - {lessons[i][0]}\n'
 
     return ret_text
